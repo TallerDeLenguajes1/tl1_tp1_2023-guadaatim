2f8a8098935b0f011c2e7fb0a393447b0e8b25be
Guadalupe Atim <guadiiatim1309@gmail.com>
2023-03-23 01:28:08 -0300

    ejercicio e - agregar puntero

--- tp1_1.c
+++ tp1_1.c
@@ -4,5 +4,19 @@ int main(){
 
     printf("Hola mundo!");
 
+    int *puntero;
+    int x = 13;
+
+    puntero = &x;
+
+    printf("\ncontenido del puntero: %d", puntero);
+    printf("\ndireccion de memoria almacenada por el puntero: %d", puntero);
+    printf("\ndireccion de memoria de la variable: %d", &x);
+    printf("\ndireccion de memoria del puntero: %d", &puntero);
+    printf("\ntamaño de memoria utilizado por la variable: %d", sizeof(x));
+
+
+
+
     return 0;
 }
