09b5aa1b424450d00c36f1f3b8a63205ccd5daa9
Guadalupe Atim <guadiiatim1309@gmail.com>
2023-03-26 01:12:09 -0300

    corrigiendo funciones tipo void

--- tp1_2.c
+++ tp1_2.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 int cuadradoNumero(int num);
-void cuadradoVoid(int num);
-void invertir(int a, int b);
-void orden(int a, int b);
+void cuadradoVoid(int *num);
+void invertir(int *a, int *b);
+void orden(int *a, int *b);
 
 int main(){
 
     int numero, cuadrado;
+    int *pnum;
+
+    pnum = &numero;
 
     printf("ingrese un numero: ");
     scanf("%d",&numero);
@@ -16,9 +19,13 @@ int main(){
 
     printf("\ncuadrado del numero: %d", cuadrado);
 
-    cuadradoVoid(numero);
+    cuadradoVoid(pnum);
 
     int a, b;
+    int *punteroa, *punterob;
+
+    punteroa = &a;
+    punterob = &b;
     
     printf("\ningrese otro numero: ");
     scanf("%d",&a);
@@ -26,9 +33,15 @@ int main(){
     scanf("%d",&b);
 
     printf("\na = %d, b = %d", a, b);
-    invertir(a,b);
-    orden(a,b);
+    
+    invertir(punteroa, punterob);
+    printf("\ninvertidos: ");
+    printf("\na = %d",a);
+    printf("\nb = %d", b);
 
+    orden(punteroa, punterob);
+    printf("\nvalor mas chico: %d", a);
+    printf("\nvalor mas grande: %d", b);
 
     return 0;
 }
@@ -38,37 +51,30 @@ int cuadradoNumero(int num)
     return num*num;
 }
 
-void cuadradoVoid(int num)
+void cuadradoVoid(int *num)
 {
     printf("\ncontenido de num: %d", num);
     printf("\ndireccion de memoria de num: %d", &num);
-    printf("\ncuadrado del numero: %d", num*num);
+    printf("\ncuadrado del numero: %d", (*num)*(*num));
 }
 
-void invertir(int a, int b)
+void invertir(int *a, int *b)
 {
     int aux;
 
-    aux = a;
-    a = b;
-    b = aux;
-
-    printf("\ninvertidos: ");
-    printf("\na = %d",a);
-    printf("\nb = %d", b);
+    aux = *a;
+    *a = *b;
+    *b = aux;
 }
 
-void orden(int a, int b)
+void orden(int *a, int *b)
 {
     int aux;
 
-    if (a > b)
+    if (*a > *b)
     {
-        aux = a;
-        a = b;
-        b = aux;
+        aux = *a;
+        *a = *b;
+        *b = aux;
     }
-
-    printf("\nvalor mas chico: %d", a);
-    printf("\nvalor mas grande: %d", b);
 }
